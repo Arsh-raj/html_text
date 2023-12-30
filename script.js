a2e21015d42daa41df159bfb3ffae8d225a24728
Arsh-raj <arshraj2003@gmail.com>
2023-12-30 13:25:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+void click()
+      {
+         var name = document.getElementById('name').value
+         alert(name);
+         document.getElementById("show").innerHTML = name.value;
+         var email = document.getElementById('email').value
+         alert(email);
+         var mobilenumber = document.getElementById('mobile number').value
+         alert(mobilenumber);
+         var collegename = document.getElementById('college').value
+         alert(collagename);
+         var Nationality = document.getElementById('Nationality').value
+         alert(Nationality);
+         var Address = document.getElementById('Address').value
+         alert(Address);
+         var city = document.getElementById('city').value
+         alert(city);
+         var region = document.getElementById('region').value
+         alert(region);
+         var pincode = document.getElementById('pin code').value
+         alert(pincode);
+         var date = document.getElementById('date').value
+         alert(date);
+
+
+      }
